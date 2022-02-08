194976a2b822e5dd5e3478c545976ce7845951b1
Oh JuYeon <dpffpself@naver.com>
2022-02-08 10:34:21 +0900

    commit comment

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 import { csSel } from "./csSel.js";
 
-new csSel({
-    //option값은 줘도 되고 안 줘도 됩니다. 기본값은 모두 false에요.
+//option값은 줘도 되고 안 줘도 됩니다. 기본값은 모두 false에요.
+//(== 기존 select를 삭제하고, 보이지 않게 합니다.);
+//현재 데모에선 select를 삭제하지 않고, 보이게 하고 있습니다.
+
+const option = {
     delete : false, //기존 select를 제거하지 않습니다.
-    visible: true //기존 select를 가리지 않고 보이게 합니다. visible이 true여도, delete가 true면 visible은 false가 됩니다.
-});
+    visible: true //기존 select를 가리지 않고 보이게 합니다. ※visible이 true여도, delete가 true면 visible은 false가 됩니다.
+};
+
+new csSel(option);
